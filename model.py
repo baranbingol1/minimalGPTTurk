6ff6a800679a84db2e4c30053dc8e350376684ce
Baran Bingöl <111634390+baranbingol1@users.noreply.github.com>
2024-09-18 15:41:38 +0300

    model.py oluşturuldu
    LayerNormalization implementasyonu eklendi.

---
+++ model.py
@@ -0,0 +1,54 @@
+from typing import Union, List
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class LayerNorm(nn.Module):
+    """LayerNormalization (https://arxiv.org/abs/1607.06450) implementasyonu.
+
+    BatchNormalization'a göre ana avantajları batch_size'a bağlı olmaması ve distributed training için daha uygun olması.
+    Ana farkları ise BN'nin batch dimension boyunca normalizasyon yapması LN'nin ise feature dimension'ı boyunca normalizasyon yapmasıdır.
+    Dolayısıyla BN batch boyutuna bağlı kalmakta ve batch boyutu modelin performansını etkilemektedir.
+
+    Argümanlar:
+        normalized_shape: batch_size boyutu hariç girdinin boyutu. Tipi [int, int listesi veya torch.Size] olmalıdır.
+        eps: nümerik hesaplamayı olabilir kılmak için küçük bir değer. varsayılan : 1e-5 (float)
+        learn_scale_shift: True ise normalize edilmiş değerlere scaling ve shifting uygular. varsayılan : True (boolean)
+    """
+    def __init__(self, normalized_shape: Union[int, List[int], torch.Size], eps: float = 1e-5, learn_scale_shift: bool = True):
+        super().__init__()
+
+        if isinstance(normalized_shape, int):
+            normalized_shape = torch.Size([normalized_shape])
+        elif isinstance(normalized_shape, list):
+            normalized_shape = torch.Size(normalized_shape)
+        
+        assert isinstance(normalized_shape, torch.Size), "normalized_shape'in tipi [int, int listesi veya torch.Size] olmalıdır"
+
+        self.normalized_shape = normalized_shape
+        self.eps = eps
+        self.learn_scale_shift = learn_scale_shift
+
+        if self.learn_scale_shift:
+            # γ ve β öğrenilebilir parametreleri.
+            self.γ = nn.Parameter(torch.ones(normalized_shape))
+            self.β = nn.Parameter(torch.zeros(normalized_shape))
+
+
+    def forward(self, x: torch.Tensor):
+        """x: [seq_len, batch_size, feats] şeklinde bir girdi vektörü olmalıdır."""
+        # ortalamanın hesaplanacağı boyutlar
+        dims = [-(i + 1) for i in range(len(self.normalized_shape))] 
+        
+        mean = x.mean(dim=dims, keepdim=True)
+        mean_x2 = (x**2).mean(dim=dims, keepdim=True)
+        var = mean_x2 - mean**2 # var[x] = E[x^2] - E[x]^2
+
+        x_norm = (x - mean) / torch.sqrt(var+self.eps)
+
+        if self.learn_scale_shift:
+            x_norm = self.γ*x_norm + self.β
+        
+        return x_norm
